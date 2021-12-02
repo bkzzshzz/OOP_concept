55e8d9672268f8ae5df5c4e04387dff5f59a9dbc
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2021-12-02 18:32:38 +0545

    Use of Abstraction|Polymosphism

--- OOP_Principles/getters_and_setters/main.py
+++ OOP_Principles/getters_and_setters/main.py
@@ -1,9 +1,9 @@
-from item import Item
+from phone import Phone
+from keyboard import Keyboard
 
-item1 = Item("MyItem", 750)
+item1 = Keyboard("jscKeyboard", 1000, 3)
 
-
-item1.apply_increment(0.2)
 item1.apply_discount()
 
 print(item1.price)
+
